68f5373c6f06759324a92a8d68537059e3bbffea
moeariyoshi <moe.ariyoshi@Michaelangelo.local>
2024-02-11 23:25:00 -0500

    input + ifelse + loop

--- sample.cs
+++ sample.cs
@@ -13,5 +13,28 @@ class Program {
         Console.WriteLine($"Height: {height}");
         Console.WriteLine($"Grade: {grade}");
         Console.WriteLine($"Message: {message}");
+
+        // user input
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}!");
+
+        // if-else
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > 0) {
+            Console.WriteLine("Positive");
+        } else if (num < 0) {
+            Console.WriteLine("Negative");
+        } else {
+            Console.WriteLine("Zero");
+        }
+
+        // for-loop
+        for (int i = 1; i <= 5; i++) {
+            Console.Write($"{i} ");
+        }
     }
 }
